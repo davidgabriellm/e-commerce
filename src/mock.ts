c61fb75d62c93e0ac78559fd1a5941eb3ab0deba
Brene <brendoleitepereira@gmail.com>
2024-11-25 09:30:25 -0300

    Chore: Adicionar mock para simular get da api

---
+++ src/mock.ts
@@ -0,0 +1,68 @@
+interface Product {
+  ID: number;
+  product_name: string;
+  product_description: string;
+  product_price: number;
+  image_url: string;
+}
+
+export const mock: Product[] = [
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  {
+    "ID": 924392,
+    "product_name": "fone bluetooth",
+    "product_price": 169.90,
+    "product_description": "Fone bluetooth muito barato Neymar usou na Copa de 2022 antes do jogo contra a Croácia",
+    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+  },
+  
+]
+
