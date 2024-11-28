3e6f1b9feeea4d816b2ea38ceec43383b191a036
David Gabriel <158231328+davidgabriellm@users.noreply.github.com>
2024-11-28 10:22:08 -0300

    feat: creating promotion section

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,69 @@
 import { Banner } from './Banner.style';
 import { Button } from './Button.style';
 import { Header, Icon, Nav, Wrapper, WrapperHeader } from './Header.style';
-import{ProductCard, ProductCardContainer, ProductCardWrapper, SectionTitle } from './Main.style.tsx';
-import { Footer} from './Styled.app';
+import {
+  ProductCard,
+  ProductCardContainer,
+  ProductCardWrapper,
+  SectionTitle,
+} from './Main.style.tsx';
 import carrinho from './assets/carrinho.png';
 import perfil from './assets/perfil.png';
 import pesquisa from './assets/perquisa.png';
 import smartwatch from './assets/smartwatch.png';
-import { mock, Category, Batteries } from "./mock.ts";
+import { mock, Category, Batteries } from './mock.ts';
 import { WrapperCategory, CategoryTitle } from './SectionCategory.tsx';
-import { SectionTitleBattery, BatteryCardWrapper, BatteryCardContainer, BatteryCard } from './BestsellerBatteries.tsx';
-
+import {
+  SectionTitleBattery,
+  BatteryCardWrapper,
+  BatteryCardContainer,
+  BatteryCard,
+} from './BestsellerBatteries.tsx';
+import {
+  ContainerPromotion,
+  ContainerPayment,
+  TitlePromotion,
+  ContainerSoccerBall,
+} from './SoccerBallPromotion.tsx';
+import soccerBall from './assets/soccerBall2022.png';
+import MoneyDisplay from './MoneyDisplay.tsx';
+import CountdownTimer from './CountdownTimer.tsx';
 
 type ProductPriceProps = {
   product_price: number | string;
-}
+};
 
 function App() {
-    function ProductPrice({product_price}: ProductPriceProps) {
-      const numericPrice = typeof product_price === 'string' ? parseFloat(product_price) : product_price;
 
-      if (isNaN(numericPrice)) {
-        return <p>Preço inválido</p>
-      }
-      
-      const formatPrice = (price: number) => {
-        const [integerPart, decimalPart] = price.toFixed(2).split('.');
-        return {integerPart, decimalPart}
-      }
-    
-      const {integerPart, decimalPart} = formatPrice(numericPrice)
 
-      return (
-         <p>
-            R$
-            <p className="price-integer">{integerPart}</p>
-            <span className="price-decimal">{decimalPart}</span>
-          </p> 
+  function ProductPrice({ product_price }: ProductPriceProps) {
+    const numericPrice =
+      typeof product_price === 'string'
+        ? parseFloat(product_price)
+        : product_price;
 
-      )
+    if (isNaN(numericPrice)) {
+      return <p>Preço inválido</p>;
     }
+
+    const formatPrice = (price: number) => {
+      const [integerPart, decimalPart] = price.toFixed(2).split('.');
+      return { integerPart, decimalPart };
+    };
+
+    const { integerPart, decimalPart } = formatPrice(numericPrice);
+
+    return (
+      <p>
+        R$
+        <p className="price-integer">{integerPart}</p>
+        <span className="price-decimal">{decimalPart}</span>
+      </p>
+    );
+  }
+
+  
+
   return (
     <>
       <Wrapper>
@@ -53,7 +77,7 @@ function App() {
               <a href="#">Mouse</a>
             </Nav>
             <Icon>
-              <img src={perfil}/>
+              <img src={perfil} />
               <img src={pesquisa} />
               <img src={carrinho} />
             </Icon>
@@ -90,62 +114,122 @@ function App() {
             </div>
           </div>
         </Banner>
-        
-          <ProductCardWrapper>
-            <ProductCardContainer>
-                <div className='container-subtitle'>
-                <SectionTitle>Mais Vendidos</SectionTitle>
-                <p>Ir para os mais vendidos</p>
-                </div>
-                <div className='container-products'>
-                {mock.map((product) => 
-                  <ProductCard>
-                    <img src={product.image_url} alt={product.product_name} />
-                    <h4>{product.product_name}</h4>
-                    <ProductPrice product_price={product.product_price} />              
-                  </ProductCard>           
-                )}
-                </div>
-            </ProductCardContainer>
-          </ProductCardWrapper>
-          
-          <WrapperCategory>
-            <CategoryTitle>Compre por categoria</CategoryTitle>
-            <div className="containerCategory">
-              {Category.map((category)=> 
-                <div className='containerCategoryProduct'>
-                  <div className='containerImage'>
-                  <img src={category.image} alt={category.Name} />
-                  </div>
-                  <h3>{category.Name}</h3>
-                </div>
-              )}
+
+        <ProductCardWrapper>
+          <ProductCardContainer>
+            <div className="container-subtitle">
+              <SectionTitle>Mais Vendidos</SectionTitle>
+              <p>Ir para os mais vendidos</p>
             </div>
-          </WrapperCategory>
-              
-          <BatteryCardWrapper>
-            <BatteryCardContainer>
-                <div className='container-subtitle'>
-                <SectionTitleBattery>Mais Vendidos em baterias</SectionTitleBattery>
-                <p>Ir para os mais vendidos</p>
+            <div className="container-products">
+              {mock.map(product => (
+                <ProductCard>
+                  <img src={product.image_url} alt={product.product_name} />
+                  <h4>{product.product_name}</h4>
+                  <ProductPrice product_price={product.product_price} />
+                </ProductCard>
+              ))}
+            </div>
+          </ProductCardContainer>
+        </ProductCardWrapper>
+
+        <WrapperCategory>
+          <CategoryTitle>Compre por categoria</CategoryTitle>
+          <div className="containerCategory">
+            {Category.map(category => (
+              <div className="containerCategoryProduct">
+                <div className="containerImage">
+                  <img src={category.image} alt={category.Name} />
                 </div>
-                <div className='container-batteries'>
-                {Batteries.map((battery) => 
-                  <BatteryCard>
-                    <img src={battery.image_url} alt={battery.product_name} />
-                    <h4>{battery.product_name}</h4>
-                    <ProductPrice product_price={battery.product_price} />              
-                  </BatteryCard>           
-                )}
+                <h3>{category.Name}</h3>
+              </div>
+            ))}
+          </div>
+        </WrapperCategory>
+
+        <BatteryCardWrapper>
+          <BatteryCardContainer>
+            <div className="container-subtitle">
+              <SectionTitleBattery>
+                Mais Vendidos em baterias
+              </SectionTitleBattery>
+              <p>Ir para os mais vendidos</p>
+            </div>
+            <div className="container-batteries">
+              {Batteries.map(battery => (
+                <BatteryCard>
+                  <img src={battery.image_url} alt={battery.product_name} />
+                  <h4>{battery.product_name}</h4>
+                  <ProductPrice product_price={battery.product_price} />
+                </BatteryCard>
+              ))}
+            </div>
+          </BatteryCardContainer>
+        </BatteryCardWrapper>
+
+        <ContainerPromotion>
+          <div className="contentPromotion">
+            <TitlePromotion>PROMOÇÃO RELÂMPAGO</TitlePromotion>
+            <h3>BOLA ADIDAS COPA DO MUNDO 2022</h3>
+            <p>
+              Bola de couro, original produzida hoje, compre agora e jogue como
+              Neymar pelo Barcelona em 2015 vire um gênio da pelota!
+            </p>
+            <ContainerPayment>
+              <span>50% off</span>
+              <img />
+            </ContainerPayment>
+            <div className="containerValue">
+              <MoneyDisplay value={129.9} />
+              <span className="oldValue">R$329,90</span>
+            </div>
+            <div className="containerBuy">
+              <button>ADICIONE AO CARRINHO</button>
+              <button>COMPRE AGORA</button>
+            </div>
+          </div>
+          <div className="contentImageBall">
+            <div>
+              <span><CountdownTimer/></span>
+            </div>
+            <ContainerSoccerBall>
+              <div className="containerImageSmallBall">
+                <div className="wrapperSmallBall">
+                  <img
+                    src={soccerBall}
+                    alt="bola de futebol da copa do mundo de 2002"
+                  />
                 </div>
-            </BatteryCardContainer>
-          </BatteryCardWrapper>
-
-
-
-        <Footer>
-          <p>BrenoCommerce &copy; 2024</p>
-        </Footer>
+                <div className="wrapperSmallBall">
+                  <img
+                    src={soccerBall}
+                    alt="bola de futebol da copa do mundo de 2002"
+                  />
+                </div>
+                <div className="wrapperSmallBall">
+                  <img
+                    src={soccerBall}
+                    alt="bola de futebol da copa do mundo de 2002"
+                  />
+                </div>
+                <div className="wrapperSmallBall">
+                  <img
+                    src={soccerBall}
+                    alt="bola de futebol da copa do mundo de 2002"
+                  />
+                </div>
+              </div>
+              <div className="containerImageBigBall">
+                <div className="wrapperBigBall">
+                  <img
+                    src={soccerBall}
+                    alt="bola de futebol da copa do mundo de 2002"
+                  />
+                </div>
+              </div>
+            </ContainerSoccerBall>
+          </div>
+        </ContainerPromotion>
       </Wrapper>
     </>
   );
