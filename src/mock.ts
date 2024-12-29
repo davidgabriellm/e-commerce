e8b887c5ec2379229d39bce353409726dfa40a68
David Gabriel <158231328+davidgabriellm@users.noreply.github.com>
2024-12-29 11:47:21 -0300

    fix: change id

--- src/mock.ts
+++ src/mock.ts
@@ -7,109 +7,134 @@ interface Product {
 
 export const mock: Product[] = [
   {
-    "ID": 924392,
-    "product_name": "fone bluetooth",
-    "product_price": 169.90,
-    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+    ID: 924392,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
   },
   {
-    "ID": 924392,
-    "product_name": "fone bluetooth",
-    "product_price": 169.90,
-    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+    ID: 92414435,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
   },
   {
-    "ID": 924392,
-    "product_name": "fone bluetooth",
-    "product_price": 169.90,
-    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+    ID: 9243923,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
   },
   {
-    "ID": 924392,
-    "product_name": "fone bluetooth",
-    "product_price": 169.90,
-    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+    ID: 923412,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
   },
   {
-    "ID": 924392,
-    "product_name": "fone bluetooth",
-    "product_price": 169.90,
-    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
+    ID: 92124214,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
   },
   {
-    "ID": 924392,
-    "product_name": "fone bluetooth",
-    "product_price": 169.90,
-    "image_url": "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png"
-  }
-
-]
-
+    ID: 9243941241,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
+  },
+  {
+    ID: 92352523,
+    product_name: "fone bluetooth",
+    product_price: 169.9,
+    image_url:
+      "https://w7.pngwing.com/pngs/672/72/png-transparent-apple-watch-series-3-apple-watch-series-2-smartwatch-watches-electronics-gold-apple-watch.png",
+  },
+];
 
 export const Category = [
   {
-    "Name": "MOUSE",
-    "image": "https://www.umpoukodetudo.com.br/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/7/8/784912879_Mouse_Redragon_M987P-K_Reaping_Elite.png_1.png"
+    id: 1,
+    Name: "MOUSE",
+    image:
+      "https://www.umpoukodetudo.com.br/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/7/8/784912879_Mouse_Redragon_M987P-K_Reaping_Elite.png_1.png",
   },
   {
-    "Name": "BATERIAS",
-    "image": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    id: 2,
+    Name: "BATERIAS",
+    image:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
   {
-    "Name": "FONES BLUETOOTH",
-    "image": "https://brmotorolanew.vtexassets.com/arquivos/motoBuds065.png"
+    id: 3,
+    Name: "FONES BLUETOOTH",
+    image: "https://brmotorolanew.vtexassets.com/arquivos/motoBuds065.png",
   },
   {
-    "Name": "HEADSET",
-    "image": "https://leonora.vteximg.com.br/arquivos/ids/163350-1000-1000/headset-gamer-line-estereo-driver-preto-e-vermelho-letron-74429--1.png?v=638001565515000000"
+    id: 4,
+    Name: "HEADSET",
+    image:
+      "https://leonora.vteximg.com.br/arquivos/ids/163350-1000-1000/headset-gamer-line-estereo-driver-preto-e-vermelho-letron-74429--1.png?v=638001565515000000",
   },
   {
-    "Name": "MOUSE",
-    "image": "https://www.umpoukodetudo.com.br/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/7/8/784912879_Mouse_Redragon_M987P-K_Reaping_Elite.png_1.png"
+    id: 5,
+    Name: "MOUSE",
+    image:
+      "https://www.umpoukodetudo.com.br/media/catalog/product/cache/1/image/1000x/9df78eab33525d08d6e5fb8d27136e95/7/8/784912879_Mouse_Redragon_M987P-K_Reaping_Elite.png_1.png",
   },
   {
-    "Name": "FONES BLUETOOTH",
-    "image": "https://brmotorolanew.vtexassets.com/arquivos/motoBuds065.png"
-  }
-]
+    id: 6,
+    Name: "FONES BLUETOOTH",
+    image: "https://brmotorolanew.vtexassets.com/arquivos/motoBuds065.png",
+  },
+];
 
 export const Batteries: Product[] = [
-  
   {
-    "ID": 142392,
-    "product_name": "Bateria duracell dura 24h por dia",
-    "product_price": 129.90,
-    "image_url": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    ID: 1412412,
+    product_name: "Bateria duracell dura 24h por dia",
+    product_price: 129.9,
+    image_url:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
   {
-    "ID": 142392,
-    "product_name": "Bateria duracell dura 24h por dia",
-    "product_price": 129.90,
-    "image_url": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    ID: 1124114,
+    product_name: "Bateria duracell dura 24h por dia",
+    product_price: 129.9,
+    image_url:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
   {
-    "ID": 142392,
-    "product_name": "Bateria duracell dura 24h por dia",
-    "product_price": 129.90,
-    "image_url": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    ID: 123552,
+    product_name: "Bateria duracell dura 24h por dia",
+    product_price: 129.9,
+    image_url:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
   {
-    "ID": 142392,
-    "product_name": "Bateria duracell dura 24h por dia",
-    "product_price": 129.90,
-    "image_url": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    ID: 164637,
+    product_name: "Bateria duracell dura 24h por dia",
+    product_price: 129.9,
+    image_url:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
   {
-    "ID": 142392,
-    "product_name": "Bateria duracell dura 24h por dia",
-    "product_price": 129.90,
-    "image_url": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    ID: 145474,
+    product_name: "Bateria duracell dura 24h por dia",
+    product_price: 129.9,
+    image_url:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
   {
-    "ID": 142392,
-    "product_name": "Bateria duracell dura 24h por dia",
-    "product_price": 129.90,
-    "image_url": "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg"
+    ID: 148568,
+    product_name: "Bateria duracell dura 24h por dia",
+    product_price: 129.9,
+    image_url:
+      "https://cesconstrucao.com.br/media/catalog/product/cache/1/image/400x/9df78eab33525d08d6e5fb8d27136e95/i/m/image_4133.jpg",
   },
-  
-]
+];
