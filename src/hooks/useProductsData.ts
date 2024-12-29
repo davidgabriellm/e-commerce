77475bc4efc244ee213361d3b65d95f873bcd1aa
David Gabriel <158231328+davidgabriellm@users.noreply.github.com>
2024-12-29 11:36:50 -0300

    fix: adjusting fetch data to improve dynamism

---
+++ src/hooks/useProductsData.ts
@@ -0,0 +1,19 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import axios from "axios";
+import { ProductResponse } from "../interfaces/product-data";
+
+const fetchData = async (search: string): Promise<ProductResponse> => {
+  const API_URL = `https://api.mercadolibre.com/sites/MLB/search?q=${search}`;
+  const response = await axios.get<ProductResponse>(API_URL);
+
+  return response?.data;
+};
+
+export const useProductsData = (
+  search: string,
+): UseQueryResult<ProductResponse, Error> => {
+  return useQuery<ProductResponse, Error>({
+    queryFn: () => fetchData(search),
+    queryKey: ["product-data", search],
+  });
+};
