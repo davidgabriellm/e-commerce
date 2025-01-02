bfc903a517a07009ebca4a4dfbba49c7bfda233f
David Gabriel <158231328+davidgabriellm@users.noreply.github.com>
2025-01-01 22:31:19 -0300

    fix: the mouse components building

--- src/routes/Mouse.tsx
+++ src/routes/Mouse.tsx
@@ -1,5 +1,19 @@
+import { ContainerGrid } from '../components/cardsGrid/CardGrid.style';
+import CardGridProducts from '../components/cardsGrid/CardGridProducts';
+import { useProductsData } from '../hooks/useProductsData';
+
+
 const Mouse = () => {
-  return <div>Mouse</div>;
+  const { data } = useProductsData("mouse");
+  
+  return (
+    <ContainerGrid>
+      {data?.results.map(({id, title, thumbnail, price}) => (
+        <CardGridProducts id={id} title={title} thumbnail={thumbnail} price={price}/>
+      ))}
+    </ContainerGrid>
+  );
 };
+
 
 export default Mouse;
