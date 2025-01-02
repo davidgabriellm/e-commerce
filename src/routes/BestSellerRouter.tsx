bb6596913cac96e2fe8bff1fb2c4d62d15ff2a65
David Gabriel <158231328+davidgabriellm@users.noreply.github.com>
2025-01-02 12:01:31 -0300

    feat: creating a dynamic route of the best seller

--- src/routes/BestSellerRouter.tsx
+++ src/routes/BestSellerRouter.tsx
@@ -1,5 +1,27 @@
+import { ContainerGrid } from '../components/cardsGrid/CardGrid.style';
+import CardGridProducts from '../components/cardsGrid/CardGridProducts';
+import { useProductsData } from '../hooks/useProductsData';
+
+
+
 const BestSellerRouter = () => {
-  return <div>BestSellerRouter</div>;
+  const {data} = useProductsData('maisVendidos')
+  
+  return (
+    <div>
+      <ContainerGrid>
+        {data?.results.map(({ id, title, thumbnail, price }) => (
+          <CardGridProducts
+            id={id}
+            title={title}
+            thumbnail={thumbnail}
+            price={price}
+          />
+        ))}
+      </ContainerGrid>
+    </div>
+  );
+
 };
 
 export default BestSellerRouter;
